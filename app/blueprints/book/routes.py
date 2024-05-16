1ec6a7c490301a22db8d7599e732b913b9cd100d
AnonimowyBanan <halama.robert497@gmail.com>
2024-05-16 19:31:51 +0200

    fix: create endpoint in book transaction

--- app/blueprints/book/routes.py
+++ app/blueprints/book/routes.py
@@ -9,23 +9,21 @@ from app.extensions import db
 def create() -> jsonify:
     data = request.json
 
-    session = db.session()
-
     try:
-        book = Book(title=data.get('title'), author=data.get('author'), year=data.get('year'),
-                    publishing_house_id=data.get('publishing_house_id'))
-        session.add(book)
-        session.flush()
+        with db.session() as session:
+            session.begin_nested()
+            book = Book(title=data.get('title'), author=data.get('author'), year=data.get('year'),
+                        publishing_house_id=data.get('publishing_house_id'))
+            session.add(book)
+            session.flush()
 
-        for author_id in data.get('authors_ids'):
-            author = BookAuthor(book_id=book.id, author_id=author_id)
-            session.add(author)
+            for author_id in data.get('authors_ids'):
+                author = BookAuthor(book_id=book.id, author_id=author_id)
+                session.add(author)
 
-        session.commit()
+            session.commit()
     except Exception as e:
         session.rollback()
         return jsonify({'error': str(e)}), 400
-    finally:
-        session.close()
 
     return jsonify(book.to_dict()), 201
